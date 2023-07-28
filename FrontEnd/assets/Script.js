7c5e50074cd4c6c0001e92f366096e4f591d6839
Alice Dell'unto <alicedelluntoo@gmail.com>
2023-07-28 09:46:56 +0200

    Récupération de toutes les catégories

---
+++ FrontEnd/assets/Script.js
@@ -0,0 +1,16 @@
+// Etape 1 => récupérer toutes les catégories
+const galery = document.getElementsByClassName("gallery");
+const url = 'http://localhost:5678/api/';
+const apiWorks = url + "works";
+const apiCategories = url +"categories";
+
+async function getAllCategories () 
+{
+    const categories = await fetch(apiCategories)
+    .then(async function (c){
+      return c.json();
+    });
+    return categories;
+}
+
+// etape 2 => formatage json vers html 
