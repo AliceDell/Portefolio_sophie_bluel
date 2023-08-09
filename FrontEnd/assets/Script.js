959a4cad1aaff3e010e86c93f2835fc2af056b97
AliceDell <alicedelluntoo@gmail.com>
2023-08-09 20:06:05 +0200

    modification js

--- FrontEnd/assets/Script.js
+++ FrontEnd/assets/Script.js
@@ -1,28 +1,29 @@
-// Etape 1 => récupérer toutes les catégories
+// Function => récupérer toutes les catégories
+async function getAllCategories() {
+  const result = await fetch(apiCategories).then(async function (c){
+    return c.json();
+  });
+  return result;
+}
+
+//function Afficher les boutons
+function displayButtons(categories) {
+  const buttons = document.getElementById("buttons");
+  console.log(categories);
+  for (const category of categories) {
+    buttons.insertAdjacentHTML("beforeend",
+      `<button class="button" data-id="${category.id}">
+        ${category.name}
+        </button>
+    `) 
+  }
+  
+} 
+//variables
 const galery = document.getElementsByClassName("gallery");
 const url = 'http://localhost:5678/api/';
 const apiWorks = url + "works";
 const apiCategories = url + "categories";
+//________________________________________
 
-async function getAllCategories() {
-  const categories = await fetch(apiCategories)
-    .then(async function (c) {
-      return c.json();
-    });
-
-  return categories;
-}
-
-// etape 2 => formatage json vers html 
-
-function formatCategories(categories) {
-  const buttons = document.getElementById("buttons");
-  for (const category of categories) {
-    const button = button.insertAdjacentHTML("beforeend",
-      `<button class="button" data-id="${category.id}">
-        ${category.name}
-        </button>
-      `)
-
-  }
-}      
+displayButtons(getAllCategories()); 
