040350139810bc024f31b2b8c18e870e22376591
Alice Dell'unto <alicedelluntoo@gmail.com>
2023-08-10 17:49:01 +0200

    affichages des works

--- FrontEnd/assets/Script.js
+++ FrontEnd/assets/Script.js
@@ -1,4 +1,4 @@
-// Function => récupérer toutes les catégories
+// Function pour récupérer toutes les catégories
 async function getAllCategories() {
   try {
     const response = await fetch(apiCategories);
@@ -10,7 +10,19 @@ async function getAllCategories() {
   }
 }
 
-// function Afficher les boutons
+// Récupère tous les works
+async function getAllWorks() {
+  try {
+    const response = await fetch(apiWorks);
+    const works = await response.json();
+    return works;
+  } catch (error) {
+    console.error("Erreur lors de la récupération des works :", error);
+    return [];
+  }
+}
+
+// Fonction pour afficher les boutons des catégories
 function displayButtons(categories) {
   const buttonsContainer = document.getElementById("buttons");
   for (const category of categories) {
@@ -22,21 +34,21 @@ function displayButtons(categories) {
   }
 }
 
-//function afficher tout les works :
-function displayAllWorks(works) {
-  const allGallery = document.getElementsByClassName("gallery")[0]; 
+// Fonction pour afficher tous les works
+async function displayAllWorks() {
+  const allGallery = document.getElementsByClassName("gallery")[0];
+  const works = await getAllWorks();
+  
   for (const galleryWork of works) {
     allGallery.insertAdjacentHTML("beforeend",
       `<figure data-id="${galleryWork.categoryId}">
         <img
           src="${galleryWork.imageUrl}"
-        alt="${galleryWork.title}" />
-        <figcaption>${galleryWork.title}"</figcaption>
-      </figure>
-    `)
-    
+          alt="${galleryWork.title}" />
+        <figcaption>${galleryWork.title}</figcaption>
+      </figure>`
+    );
   }
-
 }
 
 // Variables
@@ -46,8 +58,13 @@ const apiCategories = apiURL + "categories";
 
 // Point d'entrée
 async function initialize() {
-  const categories = await getAllCategories();
-  displayButtons(categories);
+  try {
+    const categories = await getAllCategories();
+    displayButtons(categories);
+    await displayAllWorks();
+  } catch (error) {
+    console.error("Une erreur est survenue :", error);
+  }
 }
 
 initialize();
